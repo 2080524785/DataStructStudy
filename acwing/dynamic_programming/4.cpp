d7c2bbb47911da2085e24ff4266ee9cd388e4f5e
2080524785 <2080524785@qq.com>
2023-08-15 16:06:07 +0800

    4 ac

---
+++ acwing/dynamic_programming/4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+const int N = 1e2+10;
+int f[N];
+int main(){
+    int n,m,t;
+    cin>>n>>m;
+    while(n--){
+        int a,b,s;
+        cin>>a>>b>>s;
+        while(s--)
+            for(int j=1;j<=m;j++)
+                f[j] = max(f[j],f[j-a]+b);
+    }
+    cout<<f[m];
+    return 0;
+}
